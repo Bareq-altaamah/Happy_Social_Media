86664c2c8cead0691d84f56d0dfb44745f7c2dbb
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-12-18 10:25:58 +0300

    add nav graph

--- app/src/main/java/com/thechance/happysocialmedia/ui/main/MainActivity.kt
+++ app/src/main/java/com/thechance/happysocialmedia/ui/main/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.thechance.happysocialmedia.R
 import com.thechance.happysocialmedia.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,7 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     val viewModel: MainViewModel by viewModels()
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +25,17 @@ class MainActivity : AppCompatActivity() {
             binding.lifecycleOwner=this
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        binding.navigation
+            .setupWithNavController(findNavController(R.id.fragment_host))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        findNavController(R.id.fragment_host).navigateUp()
+        return true
+    }
+
 
 }
