ced4a74f57c74269d226285a0ca3dbd3c2ff17ae
Bareq <bareqaltaamah@gmail.com>
2021-12-28 21:21:40 +0300

    resolve conflict

--- app/src/main/java/com/thechance/happysocialmedia/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/thechance/happysocialmedia/ui/profile/ProfileViewModel.kt
@@ -1,9 +1,11 @@
 package com.thechance.happysocialmedia.ui.profile
 
+import androidx.lifecycle.asLiveData
 import com.thechance.happysocialmedia.data.HappySocialRepository
 import com.thechance.happysocialmedia.ui.PostInteractionListener
 import com.thechance.happysocialmedia.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 @HiltViewModel
