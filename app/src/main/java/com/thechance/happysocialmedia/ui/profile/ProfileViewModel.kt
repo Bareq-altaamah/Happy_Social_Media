b80f8d3f6c5caaff68623b0a35d9e719017e51c5
Ahmed Yusef <80788290+Ahmed93Yusef@users.noreply.github.com>
2021-12-28 21:22:31 +0300

    update post model
    * convert upvote and downVote to string
    
    * add binding adapter to check button click
    
    * resolve conflict
    
    Co-authored-by: Bareq <bareqaltaamah@gmail.com>

--- app/src/main/java/com/thechance/happysocialmedia/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/thechance/happysocialmedia/ui/profile/ProfileViewModel.kt
@@ -1,16 +1,11 @@
 package com.thechance.happysocialmedia.ui.profile
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import com.thechance.happysocialmedia.data.HappySocialRepository
-import com.thechance.happysocialmedia.domain.models.Post
 import com.thechance.happysocialmedia.ui.PostInteractionListener
 import com.thechance.happysocialmedia.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,14 +13,6 @@ class ProfileViewModel @Inject constructor(
     repository: HappySocialRepository
 ): BaseViewModel(), PostInteractionListener {
 
-    val posts = MutableLiveData<List<Post>>()
     val user = repository.getCurrentUserInfo().asLiveData(Dispatchers.IO)
-
-    init {
-        viewModelScope.launch {
-            repository.getAllPosts().collect {
-                posts.postValue(it)
-            }
-        }
-    }
+    val posts = repository.getAllPosts().asLiveData(Dispatchers.IO)
 }
