f5dba6d417fcb2565021651dd46b9dfc7eed5266
muslim <n00rALDENakel>
2021-12-29 22:08:52 +0300

    refactor tow way recycler adapter.

--- app/src/main/java/com/thechance/happysocialmedia/util/bindingAdapters/RecylerViewAdapters.kt
+++ app/src/main/java/com/thechance/happysocialmedia/util/bindingAdapters/RecylerViewAdapters.kt
@@ -1,6 +1,8 @@
 package com.thechance.happysocialmedia.util.bindingAdapters
 
 import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
 import androidx.recyclerview.widget.RecyclerView
 import com.thechance.happysocialmedia.ui.base.BaseDataItemAdapter
 import com.thechance.happysocialmedia.util.extensions.log
@@ -9,4 +11,22 @@ import com.thechance.happysocialmedia.util.extensions.log
 fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
     items.log()
     (view.adapter as? BaseDataItemAdapter<T>)?.setItems(items ?: emptyList())
+}
+
+@BindingAdapter(value = ["app:setScrollState"])
+fun setScrollState(recyclerView: RecyclerView, value: Boolean) {
+}
+
+@InverseBindingAdapter(attribute = "setScrollState", event = "onScrollListener")
+fun getScrollState(recyclerView: RecyclerView): Boolean {
+    return !recyclerView.canScrollVertically(-1)
+}
+
+@BindingAdapter("onScrollListener")
+fun onScrollListener(recyclerView: RecyclerView, onChangeListener: InverseBindingListener) {
+    recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            onChangeListener.onChange()
+        }
+    })
 }
