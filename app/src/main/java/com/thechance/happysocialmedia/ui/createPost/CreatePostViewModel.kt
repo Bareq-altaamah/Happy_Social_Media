86eb8f6092626786a868b61537c24ed7c3b3bf75
Bareq <bareqaltaamah@gmail.com>
2021-12-31 21:03:24 +0300

    fix some issues in create post design

--- app/src/main/java/com/thechance/happysocialmedia/ui/createPost/CreatePostViewModel.kt
+++ app/src/main/java/com/thechance/happysocialmedia/ui/createPost/CreatePostViewModel.kt
@@ -1,11 +1,27 @@
 package com.thechance.happysocialmedia.ui.createPost
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
 import com.thechance.happysocialmedia.data.HappySocialRepository
 import com.thechance.happysocialmedia.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 @HiltViewModel
 class CreatePostViewModel @Inject constructor(
     private val repository: HappySocialRepository
-): BaseViewModel()
+): BaseViewModel() {
+
+    val user = repository.getCurrentUserInfo().asLiveData(Dispatchers.IO)
+
+    val postContent = MutableLiveData<String>()
+
+    fun onClickPublish() {
+        //TODO: implement this
+    }
+
+    fun onClickRemoveImage(){
+        //TODO: implement this
+    }
+}
