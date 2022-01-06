1c32dc6f20ac75c5cb840742d4542cbb95b90059
Shahad Kadhim <almuhandisa.sh@gmail.com>
2022-01-06 16:33:19 +0300

    select image from gallery

--- app/src/main/java/com/thechance/happysocialmedia/ui/createPost/CreatePostViewModel.kt
+++ app/src/main/java/com/thechance/happysocialmedia/ui/createPost/CreatePostViewModel.kt
@@ -1,9 +1,12 @@
 package com.thechance.happysocialmedia.ui.createPost
 
+import android.graphics.Bitmap
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asLiveData
 import com.thechance.happysocialmedia.data.HappySocialRepository
 import com.thechance.happysocialmedia.ui.base.BaseViewModel
+import com.thechance.happysocialmedia.util.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
@@ -17,11 +20,21 @@ class CreatePostViewModel @Inject constructor(
 
     val postContent = MutableLiveData<String>()
 
+    private val _clickUploadImageEvent =MutableLiveData<Event<Boolean>>()
+    val clickUploadImageEvent: LiveData<Event<Boolean>> = _clickUploadImageEvent
+
+    val imagePost= MutableLiveData<Bitmap?>()
+
     fun onClickPublish() {
         //TODO: implement this
     }
 
     fun onClickRemoveImage(){
-        //TODO: implement this
+        imagePost.postValue(null)
     }
+
+    fun onClickUploadImage(){
+        _clickUploadImageEvent.postValue(Event(true))
+    }
+
 }
--- app/src/main/java/com/thechance/happysocialmedia/util/extensions/commonExtensions.kt
+++ app/src/main/java/com/thechance/happysocialmedia/util/extensions/commonExtensions.kt
@@ -1,6 +1,8 @@
 package com.thechance.happysocialmedia.util.extensions
 
 import android.util.Log
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import com.thechance.happysocialmedia.util.Constants
 import com.thechance.happysocialmedia.util.Event
 
@@ -11,4 +13,10 @@ fun <T> T.log(tag: String = Constants.TAG): T {
 
 fun <T> Event<T>?.observe(onEventUnhandledContent: (T) -> Unit) {
     this?.getContentIfNotHandled()?.let(onEventUnhandledContent)
+}
+
+fun <T> LiveData<Event<T>>.observeEvent(owner: LifecycleOwner, function:(T) ->Unit){
+    this.observe(owner,{
+        it.observe(function)
+    })
 }
